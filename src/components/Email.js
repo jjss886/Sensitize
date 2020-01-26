08eab5d0b82984dd2aff73723bf4b22adbeb7509
James Shen <jjss886@gmail.com>
2020-01-26 16:23:23 -0500

    Testing email functionality, revamp to handle email input

--- src/components/Email.js
+++ src/components/Email.js
@@ -5,60 +5,76 @@ import emailjs from "emailjs-com";
 class Email extends Component {
   constructor(props) {
     super(props);
-    this.state = { feedback: "", name: "Name", email: "email@example.com" };
+    this.state = {
+      feedback: "",
+      email: ""
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(evt) {
-    this.setState({ feedback: evt.target.value });
-  }
+  handleChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
 
-  handleSubmit(evt) {
+  handleSubmit = evt => {
     evt.preventDefault();
     const serviceId = "sensitized_analysis",
       templateId = "sensitized_analysis",
       userId = "user_AZj3ffxVL9YBbjm9jaQTq",
       content = {
-        // message_html: this.state.feedback,
-        // from_name: this.state.name,
-        // reply_to: this.state.email
         analysis_number: 100,
         message: this.state.feedback,
-        user_name: this.state.name,
+        user_name: this.state.email.split("@")[0],
         user_email: this.state.email
       };
 
     emailjs
       .send(serviceId, templateId, content, userId)
       .then(res => {
-        console.log("Success email -", res);
+        console.log("Email success -", res);
       })
       .catch(err => console.error("WAH ERROR -", err));
-  }
+  };
 
   render() {
     return (
-      <form className="test-mailing">
-        <h1>Let's see if it works</h1>
-        <div>
-          <textarea
-            id="test-mailing"
-            name="test-mailing"
-            onChange={this.handleChange}
-            placeholder="Post some lorem ipsum here"
+      <div className="emailFullDiv">
+        <span className="emailHeaderText">Let's see if it works</span>
+
+        <div className="emailInsideDiv">
+          <div>
+            <textarea
+              name="feedback"
+              onChange={this.handleChange}
+              placeholder="Text Here"
+              value={this.state.feedback}
+              style={{ width: "100%", height: "40px" }}
+            />
+          </div>
+
+          <input
+            className="emailAddressInput"
+            type="text"
+            name="email"
+            value={this.state.email}
+            placeholder="Email"
             required
-            value={this.state.feedback}
-            style={{ width: "100%", height: "150px" }}
+            onFocus={e => (e.target.placeholder = "")}
+            onBlur={e => (e.target.placeholder = "Email")}
+            onChange={this.handleChange}
+          />
+
+          <input
+            type="button"
+            value="Submit"
+            className="emailSubmitBtn"
+            onClick={this.handleSubmit}
           />
         </div>
-        <input
-          type="button"
-          value="Submit"
-          className="btn btn--submit"
-          onClick={this.handleSubmit}
-        />
-      </form>
+
+        <hr className="emailLineBreak" />
+      </div>
     );
   }
 }
