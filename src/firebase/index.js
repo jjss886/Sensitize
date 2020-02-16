a681b0571e075c924c620004841bce788d0a2f47
James Shen <jjss886@gmail.com>
2020-02-16 13:14:05 -0500

    updated firebase keys and presentation

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,18 +1,10 @@
 // import * as firebase from "firebase";
 import firebase from "firebase/app";
 import "firebase/database";
+import fbKeys from "../../firebaseKeys";
 
 // PERSONAL FIREBASE CONFIGURATION
-const firebaseConfig = {
-  apiKey: "AIzaSyByETasiITMQ9ZRv4TxC5Oot9oe3bcXsMk",
-  authDomain: "sensitize-56568.firebaseapp.com",
-  databaseURL: "https://sensitize-56568.firebaseio.com",
-  projectId: "sensitize-56568",
-  storageBucket: "sensitize-56568.appspot.com",
-  messagingSenderId: "662919213348",
-  appId: "1:662919213348:web:d3733ecfe14fc2721dc3b1",
-  measurementId: "G-JQLL4GNG72"
-};
+const firebaseConfig = fbKeys;
 
 // INITIALIZE FIREBASE
 firebase.initializeApp(firebaseConfig);
